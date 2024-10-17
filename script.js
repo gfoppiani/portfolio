b8e26289176a4ed8367bdf5f51947dc332af9ef3
gfoppiani <gfoppiani@iuav.it>
2024-10-17 15:58:56 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+// Toggle menu
+document.querySelector('.menu-toggle').addEventListener('click', function() {
+    const menu = document.getElementById('menu');
+    menu.classList.toggle('menu-open');
+});
